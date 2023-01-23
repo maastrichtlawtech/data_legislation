e457a1bbcdb71f1e9901c73b90985855d2d2d90d
Shashank Chakravarthy <shashank.chakravarthy@gmail.com>
2023-01-23 07:47:24 +0100

    Setup file

--- setup.py
+++ setup.py
@@ -42,15 +42,19 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
                 out.setdefault(package, []).append(prefix+name)
     return out
 
-setup(name='docassemble.Database_directive',
+setup(name='docassemble.ePrivacy',
       version='0.0.1',
-      description=('Questions to determine whether the legal protection of\
-                   databases as laid out by the Database directive are\
-                   applicable in your case.'),
-      long_description='# docassemble.database_directive\n\n[Directive\
-      96/9/EC](https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=celex%3A31996L0009)\
-of the European Parliament and of the Council of 11 March 1996 on the legal\
-protection of databases\n\n## Author\n\nSystem Administrator, admin@admin.com\n\n',
+      description=('Questions to determine whether the \
+              ePrivacy Directive applies in your case.'),
+      long_description='# docassemble.ePrivacy\n\n\
+              [Directive 2002/58/EC]\
+              (https://eur-lex.europa.eu/legal-content/EN/TXT/?uri=celex%3A32002L0058)\
+              of the European Parliament and of the\
+              Council of 12 July 2002 concerning the\
+              processing of personal data and the\
+              protection of privacy in the electronic \
+              communications sector\n\n## Author\n\n\
+              System Administrator, admin@admin.com\n\n',
       long_description_content_type='text/markdown',
       author='Gijs van Dijck',
       author_email='gijs.vandijck@maastrichtuniversity.nl',
@@ -60,6 +64,6 @@ protection of databases\n\n## Author\n\nSystem Administrator, admin@admin.com\n\
       namespace_packages=['docassemble'],
       install_requires=[],
       zip_safe=False,
-      package_data=find_package_data(where='docassemble/Database_directive/',\
-                                     package='docassemble.Database_directive'),
+      package_data=find_package_data(where='docassemble/ePrivacy/',\
+                                     package='docassemble.ePrivacy'),
      )
